ab906b09feae0b403887a7403f5f49a25f5bb027
iabbakr <abubakaribrahim603@gmail.com>
2025-06-29 21:29:57 +0100

    compiled first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("please input your guess.");
+
+    let mut guess = String::new(); //mutable variable
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guesses:  {guess}")
+
 }
